dfb38c7d04199974d38cb60d890a53bed4ef8d35
burnout87 <gabriele.barni@gmail.com>
2020-07-13 23:38:15 +0200

    new stuff

---
+++ app/api.js
@@ -0,0 +1,53 @@
+const bodyParser = require("body-parser");
+var express = require("express");
+var router = express.Router();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+router.use(cors());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+// mongodb
+const config = require("../.env.json");
+// default uri "mongodb://localhost:27017/test?retryWrites=true&w=majority&useUnifiedTopology=true"
+const uri = config.mongo.uri;
+const client = new MongoClient(uri);
+
+router.use(bodyParser.json());
+
+client.connect();
+
+async function mongoGetSm(q) {
+  var result = {};
+  try {
+    const db = client.db("sms");
+    const collection = db.collection("machines");
+    result = await collection.find(q).toArray();
+  } catch (e) {
+    console.error(e);
+  }
+  return result;
+}
+
+async function mongoInsertSm(data) {
+  try {
+    const db = client.db("sms");
+    const collection = db.collection("machines");
+    await collection.insertOne(data);
+    return 1;
+  } catch (e) {
+    console.error(e);
+    return -1;
+  }
+}
+
+router.post("/sms/add/", cors(), async function (req, res) {
+  res.send({ state: await mongoInsertSm(req.body) });
+});
+
+router.get("/sms", cors(), async function (req, res) {
+  res.send({ state: await mongoGetSm(req.body) });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const { join } = require("path");
+const api = require("./app/api.js");
 const logger = require("morgan");
+
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), "./dist/browser");
+
+app.use(logger("dev"));
+
+// my custom API
+app.use("/api/", api);
+
+app.listen(PORT, () => {
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
+});
